0214138ee30bdb49c39f59e25c6f91022c7a676d
aspirantsss9-lgtm <aspirantsss9@gmail.com>
2026-09-23 20:08:03 +0300

    Delete DjangoStore directory

--- DjangoStore/store/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StoreConfig(AppConfig):
-    """Application configuration for the store app."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "store"
-    verbose_name = "Store"
--- DjangoStore/store/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    """Return a simple application response."""
-    return HttpResponse("DjangoStore is running.")
